e2a9813681e53c036230b8c7f9ad37a6ae583e23
samane <samane.ghodrati@gmail.com>
2025-08-16 12:53:05 -0700

    add

--- components/react-testing/src/App.test/liviInput.test.js
+++ components/react-testing/src/App.test/liviInput.test.js
@@ -1,5 +1,5 @@
 import LiveInput from "../5-LiveInput/LiveInput";
-import {screen,render} from '@testing-library/react'
+import {screen,render,fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom';
 
 describe("Text component", () => {
@@ -7,5 +7,17 @@ describe("Text component", () => {
     render(<LiveInput />);
     const inputElement = screen.getByPlaceholderText("Enter a text");
     expect(inputElement).toBeInTheDocument();
+  });
+   test('test', () => {
+    render(<LiveInput />);
+    const inputElement = screen.getByRole("heading",{level:2});
+    expect(inputElement).toBeInTheDocument();
+  });
+   test('test', () => {
+    render(<LiveInput />);
+    const inputElement = screen.getByPlaceholderText("Enter a text");
+    const headingElement = screen.getByRole("heading",{level:2});
+    fireEvent.change(inputElement,{target:{ value: "Hello World"}})
+    expect(headingElement).toHaveTextContent("Hello World");
   });
 });
