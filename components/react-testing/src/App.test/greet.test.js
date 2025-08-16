da488596b0f242612ffffd91308df85f40132770
samane <samane.ghodrati@gmail.com>
2025-08-16 13:01:37 -0700

    add

---
+++ components/react-testing/src/App.test/greet.test.js
@@ -0,0 +1,16 @@
+import Greet from "../3-Greet/Greet";
+import { screen,render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+describe("Text component", () => {
+  test('test', () => {
+ 
+    render(<Greet/>);
+  const divElement = screen.getByText("hello undefined");    
+    expect(divElement).toBeInTheDocument();
+  });
+  test("renders correct text when name prop is passed", () => {
+    render(<Greet name="John" />);
+    const divElement = screen.getByText("hello John");
+    expect(divElement).toBeInTheDocument();
+  });
+});
